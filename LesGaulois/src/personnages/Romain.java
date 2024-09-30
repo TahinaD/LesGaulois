7fed2eeb49fc7a6ccbd23aa41cc14bd233086a64
Tahina <dombrowski.tahina47@gmail.com>
2024-09-30 16:42:04 +0200

    les énumérés

--- LesGaulois/src/personnages/Romain.java
+++ LesGaulois/src/personnages/Romain.java
@@ -3,11 +3,14 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement;
+	private int nbEquipement = 0;
 	
 	public Romain(String nom, int force) {
 		assert(force >= 0);
 		this.nom = nom;
 		this.force = force;
+		equipement = new Equipement[2];
 	}
 
 	public String getNom() {
@@ -34,8 +37,32 @@ public class Romain {
 		assert(force < ancienneforce);
 	}
 	
+	public void sEquiper(Equipement aEquiper) {
+		if (nbEquipement == 2) {
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+		}
+		else if (nbEquipement == 1) {
+			if (equipement[0] == aEquiper) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + aEquiper + " !");
+			} else {
+				equipement[1] = aEquiper;
+				nbEquipement++;
+				System.out.println("Le soldat " + nom + " s'équipe d'un " + aEquiper + " !");
+			}
+		}
+		else {
+			equipement[0] = aEquiper;
+			nbEquipement++;
+			System.out.println("Le soldat " + nom + " s'équipe d'un " + aEquiper + " !");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
 		minus.parler("UN GAU... UN GAUGAU...");
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 }
